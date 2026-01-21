a0906f5ca003db238cc5418ba69953bcd951a1b8
hyyh28 <384505750@qq.com>
2026-01-21 14:34:26 +0800

    Update auto_update_rho.lean

--- Optlib/Algorithm/AdaptiveADMM/Strategies/auto_update_rho.lean
+++ Optlib/Algorithm/AdaptiveADMM/Strategies/auto_update_rho.lean
@@ -1,5 +1,5 @@
 -- AUTO GENERATED Lean4 FILE
-import Optlib.Algorithm.AdaptiveADMM.Strategies.Adaptive_Strategy_Convergence
+import Optlib.Algorithm.AdaptiveADMM.GeneralAdapter
 noncomputable section
 
 def update_fun (n : ℕ) (rho : ℝ) : ℝ :=
@@ -10,7 +10,7 @@ def update_fun (n : ℕ) (rho : ℝ) : ℝ :=
   | 3 => rho * (2.0 : ℝ)
   | 4 => rho / (2.0 : ℝ)
   | 5 => rho / (2.0 : ℝ)
-  | 6 => rho
+  | _ => rho
 
 def tau_seq (n : ℕ) : ℝ :=
   match n with
@@ -20,39 +20,31 @@ def tau_seq (n : ℕ) : ℝ :=
   | 3 => 1 / ((n : ℝ) + 2)^2
   | 4 => 1 / ((n : ℝ) + 2)^2
   | 5 => 1 / ((n : ℝ) + 2)^2
-  | 6 => 0
+  | _ => 0
 
-def choice_seq (n : ℕ) : Strategy3.RhoUpdateRule :=
-  match n with
-  | 0 => .increase
-  | 1 => .decrease
-  | 2 => .increase
-  | 3 => .increase
-  | 4 => .decrease
-  | 5 => .decrease
-  | 6 => .keep
-
-def adaptable_strategy_auto : GeneralAdapter.AdaptableStrategy :=
+def adaptable_strategy_auto : AdaptableStrategy :=
 {
   update_fun := update_fun,
   tau_seq := tau_seq,
-  choice_seq := choice_seq,
-  h_tau_nonneg := by intro n; dsimp [tau_seq]; apply zero_le_one_div,
-  h_tau_summable := by apply summable_one_div_nat_pow.mpr (by norm_num),
+  h_tau_nonneg := by
+    intro n; dsimp [tau_seq];
+    split <;> nlinarith,
+  h_tau_summable := by
+    admit,
   h_update_equiv := by
-    intros n rho hρ_pos; dsimp [update_fun, tau_seq, choice_seq];
-    admit
+    intro n rho hρ_pos;
+    dsimp [update_fun, tau_seq];
+    split <;> try (left; ring)
+    <;> try (right; left; field_simp; ring)
+    <;> right; right; rfl
 }
 
 variable (fullrank₁ : Function.Injective base_admm_real.A₁)
 variable (fullrank₂ : Function.Injective base_admm_real.A₂)
 
 theorem auto_converges : ∃ x₁ x₂ y,
-  let final_admm := { base_admm_real with
-    ρₙ := fun n => Nat.rec ({admm_name}.ρₙ 0) (fun k ρ => update_fun k ρ) n
-  }
-  Convex_KKT x₁ x₂ y final_admm.toOptProblem ∧
-  Tendsto final_admm.x₁ atTop (𝓝 x₁) ∧
-  Tendsto final_admm.x₂ atTop (𝓝 x₂) ∧
-  Tendsto final_admm.y atTop (𝓝 y) := by
+  Convex_KKT x₁ x₂ y base_admm_real.toOptProblem ∧
+  Tendsto base_admm_real.x₁ atTop (𝓝 x₁) ∧
+  Tendsto base_admm_real.x₂ atTop (𝓝 x₂) ∧
+  Tendsto base_admm_real.y atTop (𝓝 y) := by
   apply GeneralAdapter.converges_from_adaptable_strategy base_admm_real base_admm_kkt_real adaptable_strategy_auto fullrank₁ fullrank₂
